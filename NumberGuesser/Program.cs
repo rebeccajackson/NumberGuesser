8bb5b5d05d9ff288982f489c879aa8204c6f8c90
Rebecca Jackson <zimbecs@gmail.com>
2019-04-30 12:17:04 +1200

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            var name = "Mike Wazouski";
+            Console.WriteLine(name);
+        }
+    }
+}
